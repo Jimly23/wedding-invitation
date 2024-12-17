449e41ce38a0af20cc24451cc33078bf48c5cfcf
Jimly Assidqi <jimlyasidqi692@gmail.com>
2024-12-17 20:06:19 +0700

    Add google maps

--- src/pages/AdinEva.jsx
+++ src/pages/AdinEva.jsx
@@ -252,6 +252,11 @@ const AdinEva = () => {
             referrerPolicy="no-referrer-when-downgrade"
             className="w-full h-[400px]"
           ></iframe> */}
+          <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3957.0820147830395!2d109.08185857500051!3d-7.344687092663995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zN8KwMjAnNDAuOSJTIDEwOcKwMDUnMDQuMCJF!5e0!3m2!1sid!2sid!4v1734440553852!5m2!1sid!2sid" style={{ border: "border-0" }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            className="w-full h-[400px]"></iframe>
         </div>
       </div>
 
